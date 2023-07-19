b4f40b9fbe02414d1f0ca8844b33722c02ec52e1
Sean Morgan <Firesean98@gmail.com>
2023-07-19 13:33:41 -0700

    Adjusted the special link SVG on the nav bar to center properly

--- src/lib/components/SideNav.svelte
+++ src/lib/components/SideNav.svelte
@@ -17,6 +17,10 @@
         @apply text-purple-one text-base font-semibold;
     }
 
+    .specialSVG {
+        @apply w-8 h-8 pb-1 inline-block;
+    }
+
 </style>
 
 <div class="sticky top-0 w-[10%] h-full bg-white px-6 overflow-x-hidden sm:w-[25%]">
@@ -102,7 +106,7 @@
             </h1>
             <a href="#" class='specialLink'>
                 <span class="hideOnMobile responsiveText">Switch accounts</span>
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-8 h-8 pb-1 lg:h-6 lg:w-6 inline-block">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="specialSVG lg:h-6 lg:w-6 ">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z" />
                 </svg>
             </a>
@@ -110,7 +114,7 @@
 
         <li class='sideNavOption'>
             <a href="#" class='specialLink responsiveText'><span class="hideOnMobile">Log out</span> 
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-8 h-8 pb-1 inline-block">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="specialSVG">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15m3 0l3-3m0 0l-3-3m3 3H9" />
                 </svg>
             </a>
