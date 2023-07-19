b21e2d0657ad6cf3da028acb839ecc22f68a73ca
Sean Morgan <Firesean98@gmail.com>
2023-07-19 13:03:36 -0700

    Added the Create Ad Button

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,18 @@
 <script>
     import CreateAdButton from '$lib/components/CreateAdButton.svelte';
     import AdCard from '$lib/components/AdCard.svelte';
+
+    let objects = ["apple", "banana", "orange", "grape", "kiwi", "boat", "car", "road", "skyscraper", "hockey", "baseball", "dancing", "fun"];
+    let adCards = [];
+
+    function createAdCard(){
+      let randomIndex = 0;
+      console.log("Creating object?");
+      const newAd = {feed: randomIndex = Math.floor(Math.random() * objects.length),
+                      story: randomIndex = Math.floor(Math.random() * objects.length)};
+      adCards = [...adCards, newAd];
+    }
+
 </script>
 
 <div class='h-full grid bg-gray-three p-3 pl-[3rem] pr-[3rem] sm:w-full'>
@@ -9,8 +21,12 @@
         <h1 class='font-bold text-3xl'>Meta Ads</h1>
         <p class='text-gray-one text-base'>Run evergreen ads on Facebook and Instagram</p>
       </div>
-      <div class="sm:absolute mt-[-1rem] sm:mt-[1rem] right-[3rem] w-[7rem] md:w-[9rem] sm:w-[12rem]">
-        <CreateAdButton />
+
+      <!-- Need to remake the CreateAdButton -->
+      <!-- svelte-ignore a11y-no-static-element-interactions -->
+      <!-- svelte-ignore a11y-click-events-have-key-events -->
+      <div class="sm:absolute mt-[-1rem] sm:mt-[1rem] right-[3rem] w-[7rem] md:w-[9rem] sm:w-[12rem]" on:click={createAdCard}>
+        <CreateAdButton/>
       </div>
     </div>
     <section class="grid mt-[-2rem] sm:mt-[-5rem] sm:grid-cols-3 gap-3 mb-[100%]">
@@ -18,6 +34,9 @@
       <AdCard feedImg="https://source.unsplash.com/random/?mouse" storyImg="https://source.unsplash.com/random/?cat"/>
       <AdCard feedImg="https://source.unsplash.com/random/?snake" storyImg="https://source.unsplash.com/random/?horse"/>
       <AdCard feedImg="https://source.unsplash.com/random/?blue" storyImg="https://source.unsplash.com/random/?computer"/>
+      {#each adCards as ad}
+        <AdCard feedImg="https://source.unsplash.com/random/?{objects[ad.feed]}" storyImg="https://source.unsplash.com/random/?{objects[ad.story]}"/>
+      {/each}
     </section>
   </div>
   
